516b166014dc98a01055fae0fb4bde530181477c
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-14 17:24:24 -0400

    Update create_data.py

--- src/python/nn/create_data.py
+++ src/python/nn/create_data.py
@@ -2,16 +2,16 @@ import numpy as np
 import json
 
 def create_data():
-  x_train = np.random.rand(100, 3)
-  y_train = np.exp(x_train[:, 0]*x_train[:, 1]-3*x_train[:, 2]+1) + 3*x_train[:, 1]**4 + 0.5*np.random.rand(100)
-  x_test = np.random.rand(30,3)
-  y_test = np.exp(x_test[:, 0]*x_test[:, 1]-3*x_test[:, 2]+1) + 3*x_test[:, 1]**4 + 0.5*np.random.rand(30)
+  x_train = np.random.rand(10, 3)
+  #y_train = np.exp(x_train[:, 0]*x_train[:, 1]-3*x_train[:, 2]+1) + 3*x_train[:, 1]**4 + 0.5*np.random.rand(100)
+  #x_test = np.random.rand(30,3)
+  #y_test = np.exp(x_test[:, 0]*x_test[:, 1]-3*x_test[:, 2]+1) + 3*x_test[:, 1]**4 + 0.5*np.random.rand(30)
   
   data_dict = {}
   data_dict['X_train'] = x_train.tolist()
-  data_dict['X_test'] = x_test.tolist()
-  data_dict['y_train'] = y_train.tolist()
-  data_dict['y_test'] = y_test.tolist()
+  #data_dict['X_test'] = x_test.tolist()
+  #data_dict['y_train'] = y_train.tolist()
+  #data_dict['y_test'] = y_test.tolist()
   data_dict['schema'] = 'data'
   
   with open('data.json','w') as f:
