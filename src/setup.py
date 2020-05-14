a1412d095919f60e657f561c4afb7c32ecfaa77a
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-14 17:49:01 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -10,9 +10,4 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ),
-    install_requires = [
-        "tensorflow",
-        "pandas",
-        "numpy"
-   ],
 )
