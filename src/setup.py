4296b043372459d2d7fa0f9a5a779f4765c8e5ad
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-14 17:26:47 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
-import os
 import setuptools
 
-
 setuptools.setup(
-    name                            = "orquestra-nn",
+    name                            = "nn",
     packages                        = setuptools.find_packages(where = "python"),
-    package_dir                     = {"" : "python"},
+    package_dir                     = {
+        "" : "python"
+    },
     classifiers                     = (
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
