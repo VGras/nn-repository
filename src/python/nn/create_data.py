6251c2cf62204218c67af9f9d327e3d891479fb9
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-14 16:59:55 -0400

    Update create_data.py

--- src/python/nn/create_data.py
+++ src/python/nn/create_data.py
@@ -3,9 +3,9 @@ import json
 
 def create_data():
   x_train = np.random.rand(100, 3)
-  y_train = np.exp(X_train[0]*X_train[1]-3*X_train[2]+1) + 3* X_train[1]**4 + 0.5*np.random.rand(100)
+  y_train = np.exp(x_train[0]*x_train[1]-3*x_train[2]+1) + 3*x_train[1]**4 + 0.5*np.random.rand(100)
   x_test = np.random.rand(30,3)
-  y_test = np.exp(X_test[0]*X_test[1]-3*X_test[2]+1) + 3* X_test[1]**4 + 0.5*np.random.rand(100)
+  y_test = np.exp(x_test[0]*x_test[1]-3*x_test[2]+1) + 3*x_test[1]**4 + 0.5*np.random.rand(100)
   
   data_dict = {}
   data_dict['X_train'] = x_train.tolist()
